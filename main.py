89d22ee38eabb12cdd9bdaad45a885d8cf52149e
Donluisdavid <luisdavidxd@hotmail.com>
2026-01-22 16:44:14 +0100

    ignorer tout dans les dossiers models et data, et sauvegarder les features d'entrainement en tant que liste

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 import sys
 from src.preprocessing import DataPreprocessor
 import pandas as pd
+import os
+import json
 
+DATA_TRAIN_PATH = "data/train_set.csv"
+DATA_VAL_PATH = "data/val_set.csv"
+MODEL_PATH = "models/carrefour_model.pkl"
 
-def run_train():
+def run_preprocessing():
     print("--- DÉMARRAGE DU PREPROCESSING ---")
-    df = pd.read_csv('data/ds_assortiment_dataset.csv')
+    df= pd.read_csv('data/ds_assortiment_dataset.csv')
     prep = DataPreprocessor()    
-    prep.prepare_data(df)
+    df, features_cols = prep.prepare_data(df)
     print("Succès : Data chargée, transformée, et loadée")
 
     print("--- SPLIT DES JEUX DE DONNEES TRAIN, VALIDATION, INFERENCE ---")
@@ -19,7 +24,30 @@ def run_train():
 
     inference_df = df[(df['date'] >= '2017-12-01')&(df['date'] <= '2017-12-31')]
     inference_df.to_csv("data/inference_set.csv", index=False)
+
+    # Sauvegarde dans un fichier JSON
+    with open("models/features_cols.json", "w") as f:json.dump(features_cols, f)
+
     print("Succès : sauvegardé")
 
+def run_training():
+    print("--- ENTRAÎNEMENT ---")
+    
+    # 1. Chargement des données
+    df_train = pd.read_csv(DATA_TRAIN_PATH)
+    df_val = pd.read_csv(DATA_VAL_PATH)
+    # 4. Entraînement
+    trainer = ModelTrainer()
+    features = prep.features_cols
+    trainer.train(train_df, val_df, features)
+
+    # 5. Sauvegarde
+    trainer.save_model(prep, MODEL_PATH)
+
+    # 6. Export pour audit
+    train_df.to_csv("data/train_set.csv", index=False)
+    val_df.to_csv("data/validation_set.csv", index=False)
+    print("✅ Pipeline terminé avec succès.")  
+      
 if __name__ == "__main__":
-        run_train()
+        run_preprocessing()
--- src/preprocessing.py
+++ src/preprocessing.py
@@ -4,11 +4,6 @@ from sklearn.preprocessing import LabelEncoder
 import joblib
 
 class DataPreprocessor:
-    def __init__(self):
-        # On crée un dictionnaire pour stocker un encodeur par colonne
-        # self.encoders = {}
-        self.features_cols = []
-        # self.cat_features = ['agency', 'sku']
 
     def prepare_data(self, 
                      df, 
@@ -52,7 +47,7 @@ class DataPreprocessor:
         # df['pct_price_regular_lag_12'] = np.where(df['price_regular_lag_12']>0,(df['price_regular']-df['price_regular_lag_12'])/df['price_regular_lag_12'],0)
 
         # On définit les colonnes que le modèle va utiliser
-        self.features_cols = [
+        features_cols = [
                 #'agency', 'sku', 
                  'month', 'is_winter', 'is_springtime', 'is_summer', 'is_automn',  'year',
                 #  'easter_day','good_friday','new_year','christmas','labor_day','independence_day','revolution_day_memorial', 
@@ -63,7 +58,7 @@ class DataPreprocessor:
                 #  'discount_in_percent'
             ]
   
-        return df
+        return df, features_cols
     
     # def data_train_val_inference(self, df): 
     #     """Prépare les données pour l'entraînement, la validation, et l'inférence"""
